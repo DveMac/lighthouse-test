5b42271c1d9c39ea7488296371b46107c8880c84
DveMac <88442+DveMac@users.noreply.github.com>
2020-09-25 17:18:22 +0200

    Add some scripts

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+window.addEventListener('load', function () {
+    console.log('loaded');
+    
+    const images = document.querySelectorAll('img');
+    for (let image of images) {
+        image.addEventListener('mouseover', function () {
+            alert('MEATY!');
+        })
+    }
+})
